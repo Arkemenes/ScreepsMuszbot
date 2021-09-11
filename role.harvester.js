44da5ec561e2a5e2608f319a8d5eb06c22feb23e
Eric Muszalska <ericmcgomes@hotmail.com>
2021-09-11 20:20:49 -0300

    Fix harvester movement if there is no val storage

--- role.harvester.js
+++ role.harvester.js
@@ -1,3 +1,5 @@
+// var roleUpgrader = require('role.upgrader');
+
 var roleHarvester = {
 
     /** @param {Creep} creep **/
@@ -5,15 +7,19 @@ var roleHarvester = {
         if(creep.store.getFreeCapacity() > 0) {
 
             var sources = creep.room.find(FIND_SOURCES);
-            
+
+            // Sort the energy sources by distance from the current creep
             sources = _.sortBy(sources, s => creep.pos.getRangeTo(s));
 
+            // If there is a valid energy source, move to and harvest it
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.memory.target = sources[0];
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
+
+            // Get a structure to store the energy
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION ||
@@ -22,10 +28,28 @@ var roleHarvester = {
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
-            if(targets.length > 0) {
+
+            if (targets.length > 0 && !creep.memory.harvesting ) {
+                creep.memory.harvesting = true;
+                creep.say('🔄 harvest');
+            }
+            else if (!targets.length && creep.memory.harvesting ){
+                creep.memory.harvesting = false;
+                creep.say('⚡ upgrade');
+            }
+
+
+            if(creep.memory.harvesting) {
+                // If there is a valid target, move to and upgrade it
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.memory.target = targets[0];
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+            else {
+                // Try to upgrade controller if there is no valid target in the room
+                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }
