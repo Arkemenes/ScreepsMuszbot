00018bfa6870f6fc106a116c88ca881b2706fe29
Eric Muszalska <ericmcgomes@hotmail.com>
2021-09-11 20:21:22 -0300

    more generality in spawn

--- role.spawn.js
+++ role.spawn.js
@@ -1,18 +1,21 @@
+var config = {
+    "harvester_number":2,
+    "builder_number":2,
+    "upgrader_number":2
+};
+
 var roleSpawn = {
 
     /** @param {StructureSpawn} spawn **/
     run: function(spawn) {
-        // console.log('h'+Memory.harvesters.length);
-        // console.log('b'+Memory.builders.length);
-        // console.log('u'+Memory.upgraders.length);
-        if (!Memory.harvesters || Memory.harvesters.length <= 2){
+        if (!Memory.harvesters || Memory.harvesters.length <= config["harvester_number"]){
             spawn.spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], Game.time, {memory: {working: false, role: 'harvester'}});
 
         }
-        else if (!Memory.builders || Memory.builders.length <= 2){
+        else if (!Memory.builders || Memory.builders.length <= config["builder_number"]){
             spawn.spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], Game.time, {memory: {working: false, role: 'builder'}});
         }
-        else if (!Memory.upgraders || Memory.upgraders.length <= 2){
+        else if (!Memory.upgraders || Memory.upgraders.length <= config["upgrader_number"]){
             spawn.spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], Game.time, {memory: {working: false, role: 'upgrader'}});
         }
     }
