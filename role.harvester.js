364137047a1a6b8dd6f6b0eb5e261ad176886ff7
Eric Muszalska <ericmcgomes@hotmail.com>
2021-09-12 12:03:43 -0300

    Collect energy from best source

--- role.harvester.js
+++ role.harvester.js
@@ -15,11 +15,42 @@ var roleHarvester = {
 
         if(!creep.memory.working) {
 
-            var target = creep.pos.findClosestByPath(FIND_SOURCES, {
-                filter: (source) => {
-                    return (source.energy > 0);
+            if (creep.memory.role == 'harvester') {
+
+                // Try to find nearest contaner with energy
+                var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: s => (s.structureType == STRUCTURE_CONTAINER) &&
+                                 s.store[RESOURCE_ENERGY] > 0
+                });
+
+                // If there is no available container, get energy from source
+                if (!target) {
+                    target = creep.pos.findClosestByPath(FIND_SOURCES, {
+                        filter: (source) => {
+                            return (source.energy > 0);
+                        }
+                    });
                 }
-            });
+            }
+            else {
+
+                // If creep is not a harvester, try to get energy from a container or storage
+                var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: s => (s.structureType == STRUCTURE_CONTAINER || s.structureType == STRUCTURE_STORAGE) &&
+                                 s.store[RESOURCE_ENERGY] > 0
+                });
+
+                // If there is no available container, get energy from source
+                if (!target) {
+                    target = creep.pos.findClosestByPath(FIND_SOURCES, {
+                        filter: (source) => {
+                            return (source.energy > 0);
+                        }
+                    });
+                }
+            }
+
+            
 
             // If there is a valid energy source, move to and harvest it
             if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
