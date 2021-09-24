38c18e07189ea76c7235281c3e6770724f06726a
Eric Muszalska <ericmcgomes@hotmail.com>
2021-09-24 12:58:08 -0300

    Fix movement on invalid path

--- role.scout.js
+++ role.scout.js
@@ -3,13 +3,19 @@ module.exports = {
     /** @param {Creep} creep */
     run: function(creep) {
 
-        // creep.suicide()
+        let targetRooms = _.sortBy(Memory.rooms[creep.room.name].exits, r => (Memory.rooms[r]) ? Memory.rooms[r].lastVisit : 0);
 
-        creep.memory.targetRoom = _.sortBy(Memory.rooms[creep.room.name].exits, r => (Memory.rooms[r]) ? Memory.rooms[r].lastVisit : 0)[0];
+        for (let targetRoom of targetRooms) {
+            creep.memory.targetRoom = targetRoom;
 
-        let exitDir = Game.map.findExit(creep.room.name, creep.memory.targetRoom);
-        let Exit = creep.pos.findClosestByPath(exitDir);
-        creep.moveTo(Exit);
+            let exitDir = Game.map.findExit(creep.room.name, creep.memory.targetRoom);
+            let Exit = creep.pos.findClosestByPath(exitDir);
+            if (creep.moveTo(Exit) == 0) {
+                break;
+            }
+
+        }
+
         
     }
 };
