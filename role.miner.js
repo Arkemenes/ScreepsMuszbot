0b5c46e97f7f8387512377a2ce40a2669d06d8b7
Arkemenes <ericmcgomes@hotmail.com>
2023-06-04 23:30:32 -0300

    Add miner role

--- role.miner.js
+++ role.miner.js
@@ -1,43 +1,66 @@
-var harvester = {
+var miner = {
     /** @param {Creep} creep **/
     run: function (creep) {
-        if (creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+        if (!creep.memory.target) {
+            let sources = creep.room.find(FIND_SOURCES);
+            for (const source of sources) {
+                if (
+                    !_.filter(
+                        Game.creeps,
+                        (creep) =>
+                            creep.memory.role == "miner" &&
+                            creep.memory.target == source.id
+                    ).length
+                ) {
+                    creep.memory.target = source.id;
+                    break;
+                }
             }
-        } else {
-            // here is the sayHello() prototype
-            creep.sayHello();
+        }
 
-            if (
-                creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) ==
-                ERR_NOT_IN_RANGE
-            ) {
-                creep.moveTo(Game.spawns["Spawn1"]);
-            }
+        if (
+            creep.harvest(Game.getObjectById(creep.memory.target)) ==
+            ERR_NOT_IN_RANGE
+        ) {
+            creep.moveTo(Game.getObjectById(creep.memory.target));
         }
     },
     // checks if the room needs to spawn a creep
     spawn: function (room) {
-        var harvesters = _.filter(
+        var miners = _.filter(
             Game.creeps,
             (creep) =>
                 creep.memory.role == "miner" && creep.room.name == room.name
         );
 
-        if (harvesters.length < 2) {
+        if (miners.length < room.find(FIND_SOURCES).length) {
             return true;
         }
     },
     // returns an object with the data to spawn a new creep
     spawnData: function (room) {
         let name = "Miner" + Game.time;
-        let body = [WORK, CARRY, MOVE];
-        let memory = { role: "miner" };
+        let body = [MOVE, WORK, WORK];
+
+        let target = undefined; // the id of the source to harvest from. undefined means that there is no source to harvest from.
+        for (const source of room.find(FIND_SOURCES)) {
+            if (
+                !_.filter(
+                    Game.creeps,
+                    (creep) =>
+                        creep.memory.role == "miner" &&
+                        creep.memory.target == source.id
+                ).length
+            ) {
+                target = source.id;
+                break;
+            }
+        }
+
+        let memory = { role: "miner", target: target };
 
         return { name, body, memory };
     },
 };
 
-module.exports = harvester;
+module.exports = miner;
--- roles.js
+++ roles.js
@@ -1,5 +1,6 @@
 let creepLogic = {
     harvester: require("./role.harvester"),
+    miner: require("./role.miner"),
     upgrader: require("./role.upgrader"),
     scout: require("./role.scout"),
 };
