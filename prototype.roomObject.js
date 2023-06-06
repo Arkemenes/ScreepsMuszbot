181e6dba74f68dcc81529cedc131805787bcb2a8
Arkemenes <ericmcgomes@hotmail.com>
2023-06-05 22:50:10 -0300

    add say to structures

--- prototype.roomObject.js
+++ prototype.roomObject.js
@@ -53,3 +53,37 @@ RoomObject.prototype.popState = function () {
 RoomObject.prototype.clearState = function () {
     this.memory.stack = undefined;
 };
+
+/// @param {String} what - message will be displayed
+RoomObject.prototype.say = function (what) {
+    this.room.visual
+        .line(this.pos.x, this.pos.y, this.pos.x + 1 - 0.2, this.pos.y - 1, {
+            // Line from object to message bubble
+            color: "#eeeeee",
+            opacity: 0.9,
+            width: 0.1,
+        })
+        .circle(this.pos, {
+            // Small dot marker at the top of object
+            fill: "#aaffaa",
+            opacity: 0.9,
+        })
+        .text(what, this.pos.x + 1, this.pos.y - 1, {
+            // Fake message, used to align background (to make black border)
+            color: "black",
+            opacity: 0.9,
+            align: "left",
+            font: "bold 0.6 Arial",
+            backgroundColor: "black",
+            backgroundPadding: 0.3,
+        })
+        .text(what, this.pos.x + 1, this.pos.y - 1, {
+            // Real message
+            color: "black",
+            opacity: 0.9,
+            align: "left",
+            font: "bold 0.6 Arial",
+            backgroundColor: "#eeeeee",
+            backgroundPadding: 0.2,
+        });
+};
