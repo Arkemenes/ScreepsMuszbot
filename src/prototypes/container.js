25f12d0a71916a9cf15b5a2bf1184165bccf5769
Arkemenes <ericmcgomes@hotmail.com>
2023-06-11 19:38:06 -0300

    Add isCollector to container and link

--- src/prototypes/container.js
+++ src/prototypes/container.js
@@ -1,7 +1,7 @@
-StructureLink.prototype.isCollector = function () {
+StructureContainer.prototype.isCollector = function () {
     if (this.memory.isCollector == undefined) {
         this.memory.isCollector =
-            this.pos.findInRange(FIND_SOURCES, 2)[0] !== undefined;
+            this.pos.findInRange(FIND_SOURCES, 1)[0] !== undefined;
     }
 
     return this.memory.isCollector;
